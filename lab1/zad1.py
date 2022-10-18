a601453d6ce18d3d4325ca8419e349cc1ba03ea1
fjemiolo <filipj@student.agh.edu.pl>
2022-10-19 01:30:18 +0200

    .

--- lab1/zad1.py
+++ lab1/zad1.py
@@ -1,35 +1,47 @@
-def kupno(artykuły, przedmiot):
-    for i in range(len(artykuły)):
-        if przedmiot == artykuły[i][0]:
-            artykuły[i][1] -= 1
+def kupno(artykuly, przedmiot, ilosc):
+    artykuly[przedmiot] -= ilosc
 
 
-def zwrot(artykuły, przedmiot):
-    for i in range(len(artykuły)):
-        if przedmiot == artykuły[i][0]:
-            artykuły[i][1] += 1
+def zwrot(artykuly, przedmiot, ilosc):
+    artykuly[przedmiot] += ilosc
 
 
 klienci = ["Jan", "Adam", "Paweł", "Jacek"]
-sklep = [["skarpety", 10], ["spodnie", 15], ["koszule", 12], ["czapki", 7]]
+sklep = {"jaja": 10, "chleb": 15, "mleko": 12, "sery": 7}
 lista_akcji = []
 
 
 if __name__ == "__main__":
     while True:
-        dane = input("<klient>, [Kupuje/Zwracam], <towar> ")
-        klient, akcja, przedmiot = dane.split()
-        if akcja == "Kupuje":
-            if przedmiot not in sklep[0]:
-                print("Nie ma takiego przedmiotu, kup coś innego")
-                continue
-            kupno(sklep, przedmiot)
-            lista_akcji.append(klient, przedmiot, 1)
-            print(f'{klient} kupuje {przedmiot}')
-            print(sklep)
-        if akcja == "Zwracam":
-            zwrot(sklep, przedmiot)
-        print(lista_akcji)
-    for i in range(len(sklep)):
-        print(f"Zostały {sklep[i][1]} {sklep[i][0]}")
+        try:
+            dane = input("<klient> [kupuje(k)/zwraca(z)] <ilość> <towar>:  ")
+            klient, akcja, ilosc, przedmiot = dane.split()
+            ilosc = int(ilosc)
+            if klient not in klienci:
+                klienci.append(klient)
 
+            if akcja == "kupuje" or akcja == "k":
+                if przedmiot not in sklep.keys():
+                    print("Nie ma takiego przedmiotu, kup coś innego")
+                    continue
+                elif (sklep[przedmiot] - ilosc) < 0:
+                    print("Nie ma wystarczającej ilości tego produktu")
+                else:
+                    kupno(sklep, przedmiot, ilosc)
+                    print(f'{klient} kupił(a) {ilosc} {przedmiot}')
+                    lista_akcji.append(f"{klient}: {ilosc} {przedmiot}")
+
+            if akcja == "zwraca" or akcja == "z":
+                zwrot(sklep, przedmiot, ilosc)
+                print(f'{klient} zwrócił(a) {ilosc} {przedmiot}')
+
+        except:
+            print(60*"-")
+            print("Wykaz klientów i kupionych przez nich towarów:")
+            lista_akcji.sort()
+            for i in range(len(lista_akcji)):
+                print(lista_akcji[i])
+            print(60 * "-")
+            print("Stan magazynu: ")
+            print(sklep)
+            break
